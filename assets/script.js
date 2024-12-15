8d40bb1853b0fbe9afabca36b4e984a1f47f1481
Adarsh <adarshsidhu83@gmail.com>
2024-12-15 11:26:04 -0500

    added javascript to project

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// const topics = "HTML, CSS, Git, JavaScript";
+// console.log(topics);
+
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (let x=0;x<topics.length;x++)
+{
+    console.log(topics[x]);
+}
+// const shapes = ["triangle", "square", "pentagon", "circle"];
+// // console.log(shapes[0]);
+// for(let x = 0; x < shapes.length; x++) {
+//     console.log(shapes[x]);
+//    }
